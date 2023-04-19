25db5d5c5e87431752c781ccb763dfa976d5cf4d
q-OMEN <2182782869@qq.com>
2023-04-19 18:45:27 +0800

    q-OMEN:commit to github-main as 2023/04/19 18:45

--- javaStudy/src/d419/demo1.java
+++ javaStudy/src/d419/demo1.java
@@ -7,12 +7,16 @@ package d419;
  * @Data:2023/4/19 18:34
  * @Description: do while test
  */
+import java.util.Scanner;
 public class demo1 {
     public static void main(String[] args){
         int i=1;
+        String sname;
+        Scanner input=new Scanner(System.in);
         do{
-            System.out.println(i);
-            i++;
-        }while(i<=100);
+            System.out.print("input u name:");
+            sname=input.next();
+        }while(!sname.equals("q"));
+        System.out.println("exit!");
     }
 }
